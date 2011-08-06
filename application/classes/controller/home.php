0592076e8d533dc110ceb047268950ddaccb6c56
Zach Smith <zrsmith92@gmail.com>
2011-08-05 23:07:05 -0500

    asdf

--- application/classes/controller/home.php
+++ application/classes/controller/home.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-echo Debug::vars($_SERVER);
+echo Debug::vars($_SERVER, PAGODA, PAGODA_DB_SOCKET, PAGODA_DB_NAME, PAGODA_DB_USER, PAGODA_DB_PASSWORD);
 class Controller_Home extends Controller_Template
 {
 	public function action_index()
